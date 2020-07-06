6c7473a9348d36c5707618389dcb008b3b257948
ashinlaurel <ashinlaurel1999@gmail.com>
2020-07-06 20:31:59 +0530

    responsive fixes

--- src/Components/Features.js
+++ src/Components/Features.js
@@ -3,7 +3,7 @@ import demophone from "../Assets/phone.png";
 const Features = () => {
   return (
     <div>
-      <div class="font-sans container w-full m-auto flex flex-col lg:flex-row justify-center mt-4 ">
+      <div class="font-sans container w-full m-auto flex flex-col lg:flex-row justify-center mt-4  ">
         <div class="-mt-20 px-20 md:px-0 md:mt-8 order-2 lg:order-1 w-full lg:w-1/4 flex flex-col items-center lg:items-end justify-center text-center lg:text-right ml-0 lg:ml-8 ">
           <div
             class="w-12 h-12 flex items-center justify-center rounded-full bg-gray-300 mb-2"
@@ -12,7 +12,7 @@ const Features = () => {
             <i className="fa fa-desktop text-2xl object-scale-down text-black"></i>
             {/* <img src={ReactIcon} className="p-2" /> */}
           </div>
-          <h2 class="text-black mb-2 font-normal">React.js</h2>
+          <h2 class="text-black mb-2 font-bold">React.js</h2>
           <p class="text-gray-700 leading-normal mb-8">
             Interactive and responsive UIs built using the React framework
           </p>
@@ -22,7 +22,7 @@ const Features = () => {
           >
             <i className="fa fa-code text-2xl object-scale-down text-black"></i>
           </div>
-          <h2 class="text-black mb-2 font-normal">ExpressJS</h2>
+          <h2 class="text-black mb-2 font-bold">ExpressJS</h2>
           <p class="text-gray-700 leading-normal mb-8">
             Fast and minimalist backends and web frameworks built using Express
           </p>
@@ -32,7 +32,7 @@ const Features = () => {
           >
             <i className="fa fa-exchange text-2xl object-scale-down text-black"></i>
           </div>
-          <h2 class="text-black mb-2 font-normal">Context Api / Redux</h2>
+          <h2 class="text-black mb-2 font-bold">Context Api / Redux</h2>
           <p class="text-gray-700 leading-normal">
             Centralised states and logic to maintain maximum performance
           </p>
@@ -52,7 +52,7 @@ const Features = () => {
           >
             <i className="fa fa-database text-2xl object-scale-down text-black"></i>
           </div>
-          <h2 class="text-black mb-2 font-normal">MongoDB / Firestore</h2>
+          <h2 class="text-black mb-2 font-bold">MongoDB / Firestore</h2>
           <p class="text-gray-700 leading-normal mb-8">
             Modern database solutions for the modern cloud era
           </p>
@@ -65,20 +65,18 @@ const Features = () => {
               aria-hidden="true"
             ></i>
           </div>
-          <h2 class="text-black mb-2 font-normal">Socket.io</h2>
+          <h2 class="text-black mb-2 font-bold">Socket.io</h2>
           <p class="text-gray-700 leading-normal mb-8">
             Applications with real-time data transfer ability
           </p>
           <div
-            class="w-12 h-12 flex items-center justify-center rounded-full bg-gray-300 mb-2"
+            class="w-12 h-12 flex items-center justify-center rounded-full bg-gray-300 mb-2 "
             alt=""
           >
             <i className="fa fa-cloud text-2xl object-scale-down text-black"></i>
           </div>
-          <h2 class="text-black mb-2 font-normal">
-            AWS ec2 / Firebase Hosting
-          </h2>
-          <p class="text-gray-700 leading-normal">
+          <h2 class="text-black mb-2 font-bold ">AWS ec2 / Firebase Hosting</h2>
+          <p class="text-gray-700 leading-normal border-b border-gray-500 pb-6 sm:pb-0 sm:border-b-0">
             Secure and scalable deployment solutions
           </p>
         </div>
